1d8691244b8d7bf6f1c7a223705f4d5480280480
Francisco Peters <francisco.peters91@gmail.com>
2019-06-04 14:24:42 -0400

    added function to send messages on Slack

---
+++ slack.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+webhook_url = '' #Paste your Webhook URL here
+slack_data = {'text': "Hello World!"} #Write your message here
+
+response = requests.post(
+    webhook_url, data=json.dumps(slack_data),
+    headers={'Content-Type': 'application/json'}
+)
+if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+)
